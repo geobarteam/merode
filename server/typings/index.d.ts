59096d869de16836e4a05c2a80e87bcc59ed007b
Geoffrey Vandiest <geoffrey@Geoffreys-Air.lan>
2016-06-04 16:33:57 +0200

    configured server npm modules and typings

---
+++ server/typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/body-parser/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
+/// <reference path="modules/mime/index.d.ts" />
