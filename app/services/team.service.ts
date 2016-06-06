79fbe224834185f28515ce0c2f79c28bc83050d6
Geoffrey Vandiest <geoffrey@Geoffreys-Air.lan>
2016-06-06 20:50:36 +0200

    setup lite and api

--- app/services/team.service.ts
+++ app/services/team.service.ts
@@ -1,16 +1,31 @@
-import { Injectable } from '@angular/core';
-import { PLAYERS } from './mock-players';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
 import { Player } from '../../model/player';
+import 'rxjs/add/operator/toPromise';
+
+
 
 @Injectable()
 export class TeamService {
-    getPlayers(teamName:string):Promise<Player[]>{
-        return Promise.resolve(PLAYERS);
-    }
-    
-    getPlayer(id: number) {
-  return Promise.resolve(PLAYERS).then(
-      heroes => heroes.filter(hero => hero.id === id)[0]
-  );
-}
+
+  private playerUrl = 'http://localhost:3000/player';  // URL to web api
+
+  constructor(private http: Http) { }
+
+  getPlayers(teamName:string): Promise<Player[]> {
+    return this.http.get(this.playerUrl)
+               .toPromise()
+               .then(response => response.json().data)
+               .catch(this.handleError);
+  }
+
+  getPlayer(id: number) {
+    return this.getPlayers("dummy")
+               .then(players => players.filter(player => player.id === id)[0]);
+  } 
+
+  private handleError(error: any) {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
 }
