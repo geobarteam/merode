c9d6e30fa486d17a5bd94947acaa3d37152a4832
Geoffrey Vandiest <geoffrey@Geoffreys-Air.lan>
2016-07-02 13:57:43 +0200

    lite configured to serve test pages

--- gulpfile.js
+++ gulpfile.js
@@ -5,46 +5,76 @@ var ts = require('gulp-typescript');
 var del = require('del');
 var concat = require('gulp-concat')
 var runSequence = require('run-sequence');
-var merge = require ('merge-stream');
+var merge = require('merge-stream');
 
-var libPath= './dist/public/lib' ,
-angularPath = libPath + '/@angular',
-rxjsPath = libPath + '/rxjs',
-appjsPath = './dist/apublic/app/**/*.{js,js.map}',
-cssPath = './dist/public/css';
+var libPath = './dist/public/lib',
+  angularPath = libPath + '/@angular',
+  rxjsPath = libPath + '/rxjs',
+  appjsPath = './dist/apublic/app/**/*.{js,js.map}',
+  cssPath = './dist/public/css',
+  testPath = './dist/public/test';
 
 
 gulp.task('default', ['build:server',
-                      'copy:lib',
-                      'copy:bootstrap',
-                      'copy:client', 
-                      'build:client']);
+  'copy:lib',
+  'copy:bootstrap',
+  'copy:client',
+  'build:client', 
+  'test']);
 
-gulp.task('clean', function(){
-    return del('dist')
+gulp.task('clean', function () {
+  return del('dist')
+});
+
+gulp.task('test', ['build:test', 'copy:test']);
+
+gulp.task('build:client', function () {
+  var tsProject = ts.createProject('client/tsconfig.json');
+  var tsResult = gulp.src('client/**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject))
+  return tsResult.js
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/public'))
 });
 
 gulp.task('build:server', function () {
-	var tsProject = ts.createProject(path.resolve('./server/tsconfig.json'));
+  var tsProject = ts.createProject(path.resolve('./server/tsconfig.json'));
   var tsResult = gulp.src('./server/**/*.ts')
-		.pipe(sourcemaps.init())
-        .pipe(ts(tsProject))
-	return tsResult.js
-        .pipe(sourcemaps.write()) 
-		.pipe(gulp.dest('dist'))
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject))
+  return tsResult.js
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'))
 });
 
+gulp.task('build:test', function () {
+  var tsProject = ts.createProject(path.resolve('./test/tsconfig.json'));
+  var tsResult = gulp.src('./test/**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject))
+  return tsResult.js
+    .pipe(concat('specs.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(testPath))
+});
+
+gulp.task('copy:test', function () {
+  gulp.src('./node_modules/jasmine-core/lib/jasmine-core/**/*', { base: './node_modules/jasmine-core/lib/jasmine-core' })
+    .pipe(gulp.dest(testPath + '/lib/jasmine-core'));
+    gulp.src('./test/index.html').pipe(gulp.dest(testPath));
+})
 
 gulp.task('copy:lib', function () {
-  
-  var angular = gulp.src('./node_modules/@angular/**/*.js', {base: './node_modules/@angular'})
-                .pipe(gulp.dest(angularPath));
-  var rxjs = gulp.src('./node_modules/rxjs/**/*.js', {base: './node_modules/rxjs'})
-                .pipe(gulp.dest(rxjsPath));
-                
-  var mongoose = gulp.src('./node_modules/mongoose/**/*.js', {base: './node_modules/@angular'})
-                .pipe(gulp.dest(libPath +'/mongoose'));
-                
+
+  var angular = gulp.src('./node_modules/@angular/**/*.js', { base: './node_modules/@angular' })
+    .pipe(gulp.dest(angularPath));
+  var rxjs = gulp.src('./node_modules/rxjs/**/*.js', { base: './node_modules/rxjs' })
+    .pipe(gulp.dest(rxjsPath));
+
+  var mongoose = gulp.src('./node_modules/mongoose/**/*.js', { base: './node_modules/mongoose' })
+    .pipe(gulp.dest(libPath + '/mongoose'));
+
   gulp.src('./node_modules/core-js/client/shim.min.js').pipe(gulp.dest(libPath + '/core-js/client'))
   gulp.src('./node_modules/zone.js/dist/zone.js').pipe(gulp.dest(libPath + '/zone.js/dist'))
   gulp.src('./node_modules/reflect-metadata/Reflect.js').pipe(gulp.dest(libPath + '/reflect-metadata'))
@@ -55,31 +85,28 @@ gulp.task('copy:lib', function () {
   return merge(angular, rxjs);
 });
 
-gulp.task('copy:bootstrap', function(){
-  var bootstrap = './node_modules/bootstrap/dist'; 
+gulp.task('copy:bootstrap', function () {
+  var bootstrap = './node_modules/bootstrap/dist';
   gulp.src(bootstrap + '/css/bootstrap.min.css').pipe(gulp.dest(cssPath));
   gulp.src(bootstrap + '/js/bootstrap.min.js').pipe(gulp.dest(libPath + '/bootstrap'));
 
 });
 
-gulp.task('copy:client', function(){
+gulp.task('copy:client', function () {
   var content = gulp.src('./client/**/*')
     .pipe(gulp.dest('dist/public'));
 });
 
-gulp.task('build:client', function(){
-    var tsProject = ts.createProject('client/tsconfig.json');
-    var tsResult = gulp.src('client/**/*.ts')
-		.pipe(sourcemaps.init())
-        .pipe(ts(tsProject))
-	return tsResult.js
-        .pipe(sourcemaps.write()) 
-		.pipe(gulp.dest('dist/public'))
+gulp.task('watch', ['watch:client', 'watch:test']);
+
+gulp.task('watch:client', function () {
+  gulp.watch('client/**/*', ['copy:client', 'build:client']);
 });
 
-gulp.task('watch:client', function(){
-  gulp.watch('client/**/*', ['copy:client', 'build:client']);
+gulp.task('watch:test', function () {
+  gulp.watch('test/**/*', ['test']);
 })
+
 
 
 
