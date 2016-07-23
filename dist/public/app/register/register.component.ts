30c6d009e2f9a09bd9d929feebbfb838e94b1be6
Geoffrey Vandiest <gvd8@hotmail.com>
2016-07-23 09:39:58 +0200

    form working with validation hack

--- dist/public/app/register/register.component.ts
+++ dist/public/app/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../model/user/user';
 import { UserService } from '../services/user.service'
 import { Validators } from '@angular/common';
-import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -12,31 +12,26 @@ import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@
 })
 export class RegisterComponent implements OnInit {
      
-     myForm: FormGroup;
+     public myForm: FormGroup;
      user:User;
      step=1;
      submitted = false;
      emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
      events: any[] = [];
-
-    constructor(private userService:UserService, private _fb: FormBuilder){ }
+     static That: RegisterComponent;
+    constructor(private userService:UserService, private _fb: FormBuilder){ 
+      RegisterComponent.That = this;
+    }
 
      ngOnInit(){
+       
        this.myForm = this._fb.group({
             email: ['', [<any>Validators.required, <any>Validators.pattern(this.emailRegex.source)]],
-            password: ['', [<any>Validators.required, <any>Validators.minLength(5)]],
+            password: ['', [<any>Validators.required, <any>Validators.minLength(6)]],
+            password2: ['', [<any>Validators.required]],
         })
-        this.subcribeToFormChanges();
-        
      }
-     subcribeToFormChanges() {
-        const myFormStatusChanges$ = this.myForm.statusChanges;
-        const myFormValueChanges$ = this.myForm.valueChanges;
-        myFormStatusChanges$.subscribe(x => this.events.push({ event: 'STATUS_CHANGED', object: x }));
-        myFormValueChanges$.subscribe(x => this.events.push({ 
-          event: 'VALUE_CHANGED', object: x 
-        }));
-    }
+     
 
      save(user:User, isValid: boolean){  
         this.userService.registerUser(this.user).then((result)=>{
